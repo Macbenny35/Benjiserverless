1443395af116fe927b91038be30ded304a1b3ad9
Benjaz <83968900+Macbenny35@users.noreply.github.com>
2023-02-13 14:26:13 -0500

    Udated code for gateway
    API updated

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -26,7 +26,7 @@ const MAX_IMAGE_SIZE = 1000000
 /* ENTER YOUR ENDPOINT HERE
    FILES UPLOADED TO MY ENDPOINT ARE AUTOMATICALLY DELETED EVERY FEW HOURS */
 
-const API_ENDPOINT = ' https://mlh0n2bllc.execute-api.us-east-1.amazonaws.com/default/Benjazlambda'
+const API_ENDPOINT = 'https://978zyafc97.execute-api.us-east-1.amazonaws.com/default/lambdas3ben'
 
 export default {
   name: 'app',
