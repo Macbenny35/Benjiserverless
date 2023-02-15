4249e39c65f05f4ee7ad4ff58c2f7166dce6e737
Benjaz <83968900+Macbenny35@users.noreply.github.com>
2023-02-15 12:38:35 -0500

    update code
    code updated

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -26,7 +26,7 @@ const MAX_IMAGE_SIZE = 1000000
 /* ENTER YOUR ENDPOINT HERE
    FILES UPLOADED TO MY ENDPOINT ARE AUTOMATICALLY DELETED EVERY FEW HOURS */
 
-const API_ENDPOINT = 'arn:aws:lambda:us-east-1:987192913539:function:roselambdas3'
+const API_ENDPOINT = 'https://g9xvetopgh.execute-api.us-east-1.amazonaws.com/default/johnsonlambdas3'
 
 export default {
   name: 'app',
